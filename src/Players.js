8fa2c1e0446f726a84134cda93cf9ab0517021e3
Braeden <braedenleefoster@gmail.com>
2016-04-28 19:57:55 +1200

    Next build

--- src/Players.js
+++ src/Players.js
@@ -3,9 +3,8 @@ module.exports = {
   horror: require('./players/horror.js'),
   xmontoya: require('./players/xmontoya.js'),
   margeux: require('./players/margeux.js'),
-  flippycube: require('./players/flippycube.js')
-  margeux: require('./players/margeux.js'),
-  Ψ: require('./players/psiphi.js'),
+  flippycube: require('./players/flippycube.js'),
+  psiphi: require('./players/psiphi.js'),
   potato: require('./players/potato.js'),
   stayradiated: require('./players/stayradiated.js'),
   java4eva: require('./players/java4eva.js'),
