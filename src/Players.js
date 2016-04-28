34d21d8e0fd0c63fa3a358f08b063076d1ca885c
Gerard Chin <gerardchin91@gmail.com>
2016-04-28 19:21:36 +1200

    meme

--- src/Players.js
+++ src/Players.js
@@ -2,5 +2,6 @@ module.exports = {
   siegfried: require('./players/siegfried.js'),
   horror: require('./players/horror.js'),
   xmontoya: require('./players/xmontoya.js'),
-  margeux: require('./players/margeux.js')
+  margeux: require('./players/margeux.js'),
+  grrd: require('./players/grrd.js')
 }
