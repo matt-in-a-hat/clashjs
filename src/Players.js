3f7567f3943e10647d06a36fa872b5dab5463137
sakiko_ <greens123123@gmail.com>
2016-04-28 19:32:21 +1200

    'src/players/sakiko.js'

--- src/Players.js
+++ src/Players.js
@@ -1,6 +1,7 @@
 module.exports = {
   siegfried: require('./players/siegfried.js'),
   horror: require('./players/horror.js'),
+  sakiko: require('./players/sakiko.js'),
   xmontoya: require('./players/xmontoya.js'),
   margeux: require('./players/margeux.js'),
   stayradiated: require('./players/stayradiated.js')
