c99533a4bae244643bc5cf55ac7324e87fb1fd94
Braeden <braedenleefoster@gmail.com>
2016-04-28 20:06:37 +1200

    Fixes

--- src/Players.js
+++ src/Players.js
@@ -1,18 +1,12 @@
 module.exports = {
-  siegfried: require('./players/siegfried.js'),
-  horror: require('./players/horror.js'),
   sakiko: require('./players/sakiko.js'),
   xmontoya: require('./players/xmontoya.js'),
-  margeux: require('./players/margeux.js'),
   flippycube: require('./players/flippycube.js'),
   psiphi: require('./players/psiphi.js'),
-  flippycube: require('./players/flippycube.js'),
-  margeux: require('./players/margeux.js'),
   Ψ: require('./players/psiphi.js'),
   potato: require('./players/potato.js'),
   stayradiated: require('./players/stayradiated.js'),
   java4eva: require('./players/java4eva.js'),
-  paypr: require('./players/paypr.js')
   paypr: require('./players/paypr.js'),
   wayneholis: require('./players/wayneholis.js'),
   grrd: require('./players/grrd.js')
