34cce0180fdb8991095a533ee808b9f7e0ff40de
Ro Savage <rowan.savage@gmail.com>
2016-04-28 19:39:08 +1200

    rororor

--- src/players/java4eva.js
+++ src/players/java4eva.js
@@ -1,6 +1,49 @@
 var utils = require('../lib/utils.js');
 var noAmmo = true;
-var margeux = {
+var DIRECTIONS = ['north', 'east', 'south', 'west'];
+
+
+var turnToKill = function(player, enemies) {
+  var turn = false;
+  var mockState = JSON.parse(JSON.stringify(player));
+
+  DIRECTIONS.forEach(function(direction) {
+    mockState.direction = direction;
+
+    if (utils.canKill(mockState, enemies)) {
+      turn = direction;
+    }
+  });
+
+  return turn;
+};
+
+
+var getDangerousEnemies = function(enemies) {
+  var dangerous = enemies.filter((enemy) => enemy.ammo);
+  if (dangerous.length) return dangerous;
+  return enemies;
+};
+
+var getClosestAmmo = function(player, ammoPosition) {
+  var closest;
+
+  if (!ammoPosition.length) return;
+
+  closest = ammoPosition[0];
+
+  ammoPosition.forEach(function(ammo) {
+    var isCloser = utils.getDistance(player.position, ammo) < utils.getDistance(player.position, closest);
+    if (isCloser) {
+      closest = ammo;
+    }
+  });
+
+  return closest;
+};
+
+
+var java4eva = {
   info: {
     name: 'Java4Eva',
     style: 2
@@ -12,24 +55,26 @@ var margeux = {
       noAmmo = false;
     }
     if (playerState.ammo == 0){
-      noAmmo= true;
+      noAmmo = true;
     }
 
     if (gameEnvironment.ammoPosition.length && noAmmo) {
-      directionToAmmo = utils.fastGetDirection(playerState.position, gameEnvironment.ammoPosition[0]);
+      directionToAmmo = getClosestAmmo(playerState, gameEnvironment.ammoPosition[0])
 
       if (directionToAmmo !== playerState.direction) return directionToAmmo;
       return 'move';
     }
 
+    var dangers = getDangerousEnemies(enemiesStates)
+
     //LOOK for an enemy
     if(!noAmmo){
-      directionToEnemy = utils.fastGetDirection(playerState.position, enemiesStates[0].position);
+      directionToEnemy = utils.fastGetDirection(playerState.position, dangers[0].position);
       if (directionToEnemy !== playerState.direction){
-        var directionToMargeus = utils.fastGetDirection(enemiesStates[0].position, playerState.position);
-        if (directionToMargeus !== enemiesStates[0].position){
+        var directionToMargeus = utils.fastGetDirection(dangers[0].position, playerState.position);
+        if (directionToMargeus !== dangers[0].position){
                   return directionToEnemy;
-        }else{
+        } else {
                   return utils.safeRandomMove();
         }
       }
@@ -44,14 +89,16 @@ var margeux = {
     if (utils.canKill(playerState, enemiesStates) && playerState.ammo) {
       return 'shoot';
     }
+
     if (gameEnvironment.ammoPosition.length ) {
-      directionToAmmo = utils.fastGetDirection(playerState.position, gameEnvironment.ammoPosition[0]);
+      directionToAmmo = getClosestAmmo(playerState, gameEnvironment.ammoPosition[0])
 
       if (directionToAmmo !== playerState.direction) return directionToAmmo;
       return 'move';
     }
+
     return utils.safeRandomMove();
   }
 };
 
-module.exports = margeux;
+module.exports = java4eva;
